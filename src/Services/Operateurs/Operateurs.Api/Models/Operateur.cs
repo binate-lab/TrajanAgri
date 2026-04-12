979daa29eb14cd5973fc5877783c99b11089d56d
KEITA <keita_amara@hotmail.com>
2026-04-12 02:03:51 +0000

    Initial creation models

---
+++ src/Services/Operateurs/Operateurs.Api/Models/Operateur.cs
@@ -0,0 +1,14 @@
+﻿namespace Operateurs.Api.Models;
+
+public class Operateur
+{
+    public Guid Id { get; set; }
+    public string CodeOp { get; set; } = null!;
+    public string Nom { get; set; } = null!;
+    public string Prenoms { get; set; } = null!;
+    public string? Email { get; set; }
+    public string Telephone { get; set; } = null!;
+    public bool EstCooperateur { get; set; } = false;
+    public string? Localite { get; set; }
+
+}
